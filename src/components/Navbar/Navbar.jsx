72a6e22d7f50402929bc4ef6d75f9edd5d453b81
Aditya <adityakatare0411007@gmail.com>
2026-07-21 15:54:14 +0530

    Fixed mobile responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,54 @@
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "./Navbar.css";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
 
       <div className="logo">AK</div>
 
-      <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#skills">Skills</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
+      <div
+        className="menu-icon"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+      <ul className={menuOpen ? "nav-links active" : "nav-links"}>
+
+        <li>
+          <a href="#home" onClick={() => setMenuOpen(false)}>
+            Home
+          </a>
+        </li>
+
+        <li>
+          <a href="#about" onClick={() => setMenuOpen(false)}>
+            About
+          </a>
+        </li>
+
+        <li>
+          <a href="#skills" onClick={() => setMenuOpen(false)}>
+            Skills
+          </a>
+        </li>
+
+        <li>
+          <a href="#projects" onClick={() => setMenuOpen(false)}>
+            Projects
+          </a>
+        </li>
+
+        <li>
+          <a href="#contact" onClick={() => setMenuOpen(false)}>
+            Contact
+          </a>
+        </li>
+
       </ul>
 
     </nav>
